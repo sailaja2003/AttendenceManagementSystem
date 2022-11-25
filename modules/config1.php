04000f125ecedaa0aae2176dedb3180d1925c08c
sailaja2003 <99901787+sailaja2003@users.noreply.github.com>
2022-11-25 23:30:18 +0530

    Add files via upload

---
+++ modules/config1.php
@@ -0,0 +1,22 @@
+<?php
+
+	//  Development connection
+	
+	$databaseHost = 'localhost';
+	$databaseName = 'studentdatabse';
+	$databaseUsername = 'root';
+	$databasePassword = '';
+
+
+	
+	try {
+		
+		$conn = new PDO('mysql:host=' . $databaseHost . ';dbname=' . $databaseName . '', $databaseUsername, $databasePassword);
+	}
+	catch (PDOException $e) {
+    echo $e->getMessage();
+	}
+	// echo "Connection is there<br/>";
+	$conn->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+
+?>
---
+++ modules/footer.php
@@ -0,0 +1,48 @@
+</div>
+		<footer id="footer">
+			<div id="">
+			<p>&copy; 2022 Student Attendance System <br>
+
+			
+				
+			</p>
+			</div>
+			<div id="links">
+				<a>Nidhi Sheth</a> 
+				<a>Kamal Sab</a> 
+				<a>Policharla Sai Sailaja</a> 
+				<a >Vallabha N Kowshik</a> 
+			</div>
+			
+		</footer>
+		<style>
+			footer {
+			padding-top: 10px;
+			position: fixed;
+  			bottom: 0;
+  			background-color: #373435;
+  			width: 100%;
+  			text-align: center; 
+  			color: #fff;
+  			padding-bottom: 10px;
+
+			}	
+			
+
+			#footer a{
+				margin-right:10px;
+				text-decoration: none;
+				color: white;
+				padding-left:15px;
+				padding-right:15px;
+				margin-bottom:10px;
+			}
+			footer a:hover{
+				color: white;
+			}
+
+		</style>
+		<script src="js/bootstrap.min.js"></script>
+    <script src="js/custom.js"></script>
+  </body>
+</html>
